6c308a638953b31625e8caf8e0a8cd59d778603e
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-16 12:00:29 +0100

    Refactor: dark mode to toggle button

--- src/components/header/SwitchModeBtn.vue
+++ src/components/header/SwitchModeBtn.vue
@@ -1,9 +1,13 @@
 <template>
-  <q-btn flat :icon="mdiThemeLightDark" @click="() => $q.dark.toggle()" />
+  <q-toggle color="blue-grey-8" dark @click="() => $q.dark.toggle()" v-model="dark">
+    <template #default> <q-icon flat :name="mdiThemeLightDark" size="sm" /> </template>
+  </q-toggle>
 </template>
 
 <script setup lang="ts">
 import { mdiThemeLightDark } from '@quasar/extras/mdi-v7'
+import { ref } from 'vue'
+const dark = ref(false)
 </script>
 
 <style scoped></style>
