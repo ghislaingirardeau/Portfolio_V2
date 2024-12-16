0487c6416cdf334c8e611d6aef4ff47f7a048cda
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-16 11:17:18 +0100

    Responsive: drawer control device

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -1,11 +1,25 @@
 <template>
-  <q-drawer show-if-above v-model="leftDrawerOpen" side="left" behavior="mobile" bordered>
+  <q-drawer
+    show-if-above
+    v-model="leftDrawerOpen"
+    side="left"
+    :behavior="deviceOrientation"
+    bordered
+  >
     <!-- drawer content -->
   </q-drawer>
 </template>
 
 <script setup lang="ts">
+import { useScreenOrientation } from '@vueuse/core'
+import { computed } from 'vue'
+
+const { orientation } = useScreenOrientation()
+
 const leftDrawerOpen = defineModel('leftDrawerOpen', { type: Boolean, required: true })
+const deviceOrientation = computed(() => {
+  return orientation.value === 'landscape-primary' ? 'desktop' : 'mobile'
+})
 </script>
 
 <style scoped></style>
