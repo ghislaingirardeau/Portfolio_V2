b45ed1bf0bfd3cdded96febf0824518d83f951a2
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-16 10:29:26 +0100

    Init: setup project & add icons

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,6 @@
-import { defineRouter } from '#q-app/wrappers';
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router';
-import routes from './routes';
+import { defineRouter } from '#q-app/wrappers'
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
+import routes from './routes'
 
 /*
  * If not building with SSR mode, you can
@@ -19,7 +14,9 @@ import routes from './routes';
 export default defineRouter(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+      ? createWebHistory
+      : createWebHistory
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -29,7 +26,7 @@ export default defineRouter(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
-  });
+  })
 
-  return Router;
-});
+  return Router
+})
