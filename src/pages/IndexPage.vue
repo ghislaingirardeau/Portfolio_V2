2fac960186a880a30ece1af2a633dde4f5753fa8
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-17 14:44:29 +0100

    Init: add gsap library

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,14 @@
 <template>
-  <q-page class="q-pa-lg">Hello </q-page>
+  <q-page class="q-pa-lg">
+    <span>{{ t('index.me.p1') }}</span>
+    <span>{{ t('index.me.p2') }}</span>
+    <span>{{ t('index.me.p3') }}</span>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n({ useScope: 'global' })
+</script>
 
 <style scoped></style>
