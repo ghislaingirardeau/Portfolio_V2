75fbc8e628b1ef82ad0be7caedf823ca81cda446
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-01-06 16:42:21 +0100

    Perf: project - on mobile swipe carousel

--- src/components/projectPage/carouselProjects.vue
+++ src/components/projectPage/carouselProjects.vue
@@ -1,16 +1,40 @@
 <template>
-  <div class="row justify-around items-center mt-4">
-    <q-icon :name="mdiArrowLeft" @click="previousSlide" class="cursor-pointer"></q-icon>
-    <div class="w-48 h-72" :class="{ 'w-64': typeDesktop }">
+  <div
+    ref="carousel"
+    class="row justify-around items-center mt-4"
+    :class="{ relative: typeDesktop }"
+  >
+    <q-btn
+      round
+      fab-mini
+      @click="previousSlide"
+      color="secondary"
+      icon="chevron_left"
+      class="z-10 opacity-70"
+      :class="{ 'absolute left-1 ': typeDesktop }"
+    />
+    <div class="w-48 h-72" :class="{ 'w-72': typeDesktop }">
       <slot></slot>
     </div>
-    <q-icon :name="mdiArrowRight" @click="nextSlide" class="cursor-pointer"></q-icon>
+    <q-btn
+      round
+      fab-mini
+      color="secondary"
+      @click="nextSlide"
+      icon="chevron_right"
+      :class="{ 'absolute right-1 ': typeDesktop }"
+      class="z-10 opacity-70"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { mdiArrowLeft, mdiArrowRight } from '@quasar/extras/mdi-v7'
+import { useSwipe } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
 const currentSlide = defineModel('currentSlide', { type: Number, required: true })
+const carousel = ref()
+const { direction } = useSwipe(carousel)
 
 const props = defineProps({
   slideNumber: {
@@ -22,14 +46,6 @@ const props = defineProps({
     required: true,
   },
 })
-
-/* const showRightArrow = computed(() => {
-  return currentSlide.value !== props.slideNumber - 1
-})
-
-const showLeftArrow = computed(() => {
-  return currentSlide.value !== 0
-}) */
 
 function nextSlide() {
   if (currentSlide.value === props.slideNumber - 1) {
@@ -46,6 +62,17 @@ function previousSlide() {
   }
   currentSlide.value--
 }
+
+watch(
+  () => direction.value,
+  (newValue) => {
+    if (newValue === 'left') {
+      previousSlide()
+    } else if (newValue === 'right') {
+      nextSlide()
+    }
+  },
+)
 </script>
 
 <style scoped></style>
