d2626fc9d7642862e16513cdb7e9497b9f14764b
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-01-09 17:00:49 +0100

    clean code

--- src/components/common/TheRobotContainer.vue
+++ src/components/common/TheRobotContainer.vue
@@ -8,7 +8,7 @@
     <TheRobot v-if="layoutMounted" />
     <WireCode
       content="&lt;svg&gt;Un chat bot&lt;/svg&gt;"
-      class="mt-10 absolute bottom-5 right-5 lg:bottom-5 lg:right-10"
+      class="mt-10 absolute h-10 w-48 border-solid border-gray-300 bg-grey-3 bottom-5 right-5 lg:bottom-5 lg:right-10"
       v-else
     />
   </transition>
