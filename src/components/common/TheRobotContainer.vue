e21ca233bb9fd09361c333188f52a9bc09dfde5c
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-01-22 16:36:17 +0100

    Fix: robot action - available on robot is clickable

--- src/components/common/TheRobotContainer.vue
+++ src/components/common/TheRobotContainer.vue
@@ -4,7 +4,7 @@
     enter-active-class="animated fadeIn slow"
     leave-active-class="animated fadeOut slow"
   >
-    <TheRobot v-if="presentationMounted || headerMounted" @click="emit('robotAction')" />
+    <TheRobot v-if="presentationMounted || headerMounted" @click="handleRobotAction" />
     <!-- <WireCode
       content="&lt;svg&gt;Un chat bot&lt;/svg&gt;"
       class="mt-10 absolute h-10 w-48 border-solid border-gray-300 bg-grey-3 bottom-5 right-5 lg:bottom-5 lg:right-10"
@@ -20,9 +20,15 @@ import { storeToRefs } from 'pinia'
 import { useAnimationSettings } from 'src/stores/animationSettings'
 const animationSettings = useAnimationSettings()
 
-const { headerMounted, presentationMounted } = storeToRefs(animationSettings)
+const { headerMounted, presentationMounted, isRobotClickable } = storeToRefs(animationSettings)
 
 const emit = defineEmits(['robotAction'])
+
+function handleRobotAction() {
+  if (isRobotClickable.value) {
+    emit('robotAction')
+  }
+}
 </script>
 
 <style scoped></style>
