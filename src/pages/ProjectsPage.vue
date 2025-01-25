b9556a54a07b9f4363cba7fe9a02c6668c71f2fe
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-01-25 16:27:13 +0100

    Refactor: performance component loading

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,26 +1,37 @@
 <template>
-  <projectMobileView v-if="useIsMobile()" :key="locale" />
-
-  <projectDesktopView v-else />
+  <component :is="loadComponent" :key="locale" />
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import projectMobileView from 'src/components/projectPage/projectMobileView.vue'
 import { useAnimationSettings } from 'src/stores/animationSettings'
 import { useIsMobile } from 'src/utils/useDeviceInfo'
-import { onMounted } from 'vue'
-import projectDesktopView from 'src/components/projectPage/projectDesktopView.vue'
+import { computed, defineAsyncComponent, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
+
+const projectMobileView = defineAsyncComponent(
+  () => import('src/components/projectPage/projectMobileView.vue'),
+)
+const projectDesktopView = defineAsyncComponent(
+  () => import('src/components/projectPage/projectDesktopView.vue'),
+)
 
 const animationSettings = useAnimationSettings()
 const { presentationMounted, pageMounted } = storeToRefs(animationSettings)
 
 const { locale } = useI18n()
 
+const loadComponent = computed(() => {
+  if (useIsMobile()) {
+    return projectMobileView
+  } else {
+    return projectDesktopView
+  }
+})
+
 onMounted(() => {
   if (!presentationMounted.value) {
-    presentationMounted.value = true
+    // presentationMounted.value = true
   }
   pageMounted.value = false
 })
